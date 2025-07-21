ccb5204b9044689f8866e2aa94f9761d4ba15d6b
fanzhang172 <fanzhang172@gmail.com>
2025-07-21 16:30:18 -0700

    Add location service dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
   implementation(libs.androidx.navigation.compose)
   implementation(libs.androidx.hilt.navigation.compose)
   implementation(libs.androidx.lifecycle.viewmodel.compose)
+  implementation(libs.play.services.location)
   kapt(libs.hilt.compiler)
 
   testImplementation(libs.junit)
